f0cbf32f1e8b2f964bedf4a64ef4669d53be9de9
xiangke <xiangke@imdada.cn>
2019-06-29 22:56:39 +0800

    export server1

--- fastrpc-core/src/main/java/com/fast/rpc/rpc/AbstractProtocol.java
+++ fastrpc-core/src/main/java/com/fast/rpc/rpc/AbstractProtocol.java
@@ -6,8 +6,6 @@ import com.fast.rpc.util.FrameworkUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.HashMap;
-import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
 /**
